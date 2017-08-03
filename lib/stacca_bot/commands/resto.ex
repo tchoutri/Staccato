f6b55980de9fcf692e2905023db7f1e88afb0eba
Théophile Choutri <theophile@choutri.eu>
2017-08-03 22:23:39 +0200

    Now it's getting a tad more structured.
    There's now a %Resto{} struct and its associated type.
    Restaurants are now inserted into this struct, which needs the
    `carte` hierarchy so the informations about the restaurant are not
    at the same level as the menus.
    I've written a tinay wrapper around `Enum.find` to fetch the restaurants
    by name more easily.

--- lib/stacca_bot/commands/resto.ex
+++ lib/stacca_bot/commands/resto.ex
@@ -1,6 +1,17 @@
 defmodule StaccaBot.Commands.Resto do
   use StaccaBot.Commander
   use StaccaBot.Router
+  alias __MODULE__
+
+  defstruct [:carte,
+             :coordonees,
+             :description,
+             :nom
+            ]
+
+  @type t :: %__MODULE__{nom: String.t, description: String.t,
+                         coordonees: [float()], carte: map()
+                        }
 
   def resto(update) do
     {:ok, _} = send_message "Liste des restaurants en mémoire",
@@ -16,9 +27,11 @@ defmodule StaccaBot.Commands.Resto do
             callback_data: "/resto Le Train de Vie",
             text: "Le Train de Vie",
           },
+        ],
+        [
           %{
-            callback_data: "/resto Istambul Express",
-            text: "Istambul Express",
+            callback_data: "/resto Istanbul Express",
+            text: "Istanbul Express",
           },
         ]
       ]
@@ -28,7 +41,8 @@ defmodule StaccaBot.Commands.Resto do
   @spec build_resto(String.t) :: [map()]
   def build_resto(resto) when is_binary(resto) do
     load_resto()
-    |> Map.get(String.to_atom(resto))
+    |> get_resto(resto)
+    |> Map.get(:carte)
     |> Map.keys
     |> build_inline_keyboard(resto)
   end
@@ -45,18 +59,28 @@ defmodule StaccaBot.Commands.Resto do
     end
   end
 
-  @spec build(atom(), atom()) :: String.t
-  def build(cat, resto) when is_atom(resto) and
+  @spec build(atom(), String.t) :: String.t
+  def build(cat, resto) when is_binary(resto) and
                              is_atom(cat) do
     load_resto()
-    |> Map.get(resto)
+    |> get_resto(resto)
+    |> Map.get(:carte)
     |> Map.get(cat)
     |> Enum.map(fn {plat, prix} -> "• #{plat} : #{prix}€" end)
     |> Enum.join("\n")
   end
 
+  @spec load_resto() :: [Resto.t]
   def load_resto() do
     File.read!("priv/resto.toml")
     |> Tomlex.load
+    |> Enum.map(fn {nom, data} -> %Resto{carte: data.carte, coordonees: data.coordonees,
+                                         description: data.description, nom: Atom.to_string(nom)} end)
+  end
+
+  @spec get_resto([Resto.t()], String.t) :: Resto.t
+def get_resto(restos, nom) when is_list(restos) and
+                                is_binary(nom) do
+    Enum.find(restos, fn struct -> struct.nom == nom end)
   end
 end
